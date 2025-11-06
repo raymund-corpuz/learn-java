9249daf3488e13a4d65ecdaa92e8c861f4883e43
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-11-06 08:53:17 +0800

    04_temperatureConverter : Make temperature converter, use double varialbles to conver Celsius to Fahrenheit.

---
+++ 04_temperatureConverter/src/TemperatureConverter.java
@@ -0,0 +1,11 @@
+
+public class TemperatureConverter {
+	public static void main(String[] args) {
+		
+		double celsius = 30.0;
+		double fahrenheit = (celsius * 9/5) +32;
+		
+		System.out.println("Temperature in Celsius: " + celsius);
+		System.out.println("Temperature in Fahrenheit:" + fahrenheit);
+	}
+}
