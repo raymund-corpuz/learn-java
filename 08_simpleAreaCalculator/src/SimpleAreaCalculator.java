3c44edee10114151392c6ae299ded128d9556771
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-11-07 19:41:54 +0800

    08_simpleAreaCalculator: Making a Calculator that computes the area of a shape.

---
+++ 08_simpleAreaCalculator/src/SimpleAreaCalculator.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class SimpleAreaCalculator {
+	public static void main(String[] args) {
+		Scanner sc =  new Scanner(System.in);
+		
+		
+		
+		
+		
+		System.out.println("Enter length :");
+		double length = sc.nextDouble();
+		
+		System.out.println("Enter width :");
+		double width = sc.nextDouble();
+		
+		double area =  (length * width);
+		System.out.println("Area of rectangle = " + area);
+		
+		
+		
+		
+	}
+}
