409e082bcf2af20fd764299d8181b55c5e5c6775
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-11-08 21:06:53 +0800

    09_oddOrEven: Create a program that checks if the number is Odd or Even number.

--- 08_simpleAreaCalculator/src/SimpleAreaCalculator.java
+++ 08_simpleAreaCalculator/src/SimpleAreaCalculator.java
@@ -2,23 +2,13 @@ import java.util.Scanner;
 
 public class SimpleAreaCalculator {
 	public static void main(String[] args) {
-		Scanner sc =  new Scanner(System.in);
-		
-		
-		
-		
-		
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter length :");
 		double length = sc.nextDouble();
-		
 		System.out.println("Enter width :");
 		double width = sc.nextDouble();
-		
-		double area =  (length * width);
+		double area = (length * width);
 		System.out.println("Area of rectangle = " + area);
-		
-		
-		
-		
+
 	}
 }
