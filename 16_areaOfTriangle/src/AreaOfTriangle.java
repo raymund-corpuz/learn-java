2c120d73673bcb1d1ccf19f6dfb2bfff0dd2a0b0
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-11-12 20:38:51 +0800

    16_areaOfTriangle : Making a program that accepts a double as an input which is base and height then return the result of the area.

---
+++ 16_areaOfTriangle/src/AreaOfTriangle.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class AreaOfTriangle {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		double base = sc.nextDouble();
+		double height = sc.nextDouble();
+		
+		double area =  (base * height) /2;
+		
+		System.out.println(area);
+		
+		sc.close();
+	}
+}
