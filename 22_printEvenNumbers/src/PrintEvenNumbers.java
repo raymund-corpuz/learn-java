487f6d14ea1c4697ca9ca181522be0ac8310cbca
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-11-14 07:55:01 +0800

    22_printEvenNumbers : Prints all the Even numbers and then skipped all the Odd Numbers.

---
+++ 22_printEvenNumbers/src/PrintEvenNumbers.java
@@ -0,0 +1,14 @@
+
+public class PrintEvenNumbers {
+	public static void main(String[] args) {
+		
+		int n = 10;
+		
+		for(int i = 0; i < n; i++) {
+			if(i % 2 == 0) {
+				System.out.print(i + " ");
+			}
+			
+		}
+	}
+}
