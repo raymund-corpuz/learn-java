ce6d88b1c17487c080ac56e6a39a701c5d84b58c
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-11-13 09:52:27 +0800

    17_stringFormat : Introduction to String.format('My age is %d', age).

---
+++ 17_stringFormat/src/StringFormat.java
@@ -0,0 +1,10 @@
+
+public class StringFormat {
+	public static void main(String[] args) {
+		int age = 22;
+		
+		String str = String.format("My age is %d", age);
+		
+		System.out.println(str);
+	}
+}
