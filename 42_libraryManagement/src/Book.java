1749697e9c5e8b997a61cfea1f07947bd1cb5d3b
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-12-01 09:57:04 +0800

    42_libraryManagement: Created a User Class .

--- 42_libraryManagement/src/Book.java
+++ 42_libraryManagement/src/Book.java
@@ -1,4 +1,54 @@
 
 public class Book {
-
+	private String title; 
+	private String author;
+	private String isbn;
+	private boolean isAvailable; //correct
+	
+	//Constructor
+	public Book(String title, String author, String isbn) {
+		this.title = title;
+		this.author =  author;
+		this.isbn = isbn;
+		this.isAvailable = true; //correct
+	}
+	
+	//Getters
+	public String getTitle() {
+		return title; //correct
+	}
+	
+	public String getAuthor() {
+		return author; //correct
+	}
+	
+	public String getIsbn() {
+		return isbn; //correct
+	}
+	
+	public boolean isAvailable() {
+		return isAvailable; //correct
+	}
+	
+	//Methods
+	public void borrowBook() {
+		if(isAvailable) {
+			isAvailable = false;
+		}else {
+			System.out.println("Book is already borrowed"); //correct
+		}
+	}
+	
+	public void returnBook() {
+		if(!isAvailable) {
+			isAvailable = true;
+		}else {
+			System.out.println("Book is already returned"); // wrong return 
+		}
+	}
+	
+	@Override
+	public String toString() {
+		return "Title: " + title + ", Author: " + author + ", Isbn: " + isbn + ", Available: " + ( isAvailable ? " is availabel.": " not available.");
+	}
 }
