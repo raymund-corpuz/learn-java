1ffc3b38cc460659de1da7265023ddc79fa0a7ee
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-12-09 20:32:26 +0800

    45_enums: Combining constants with enum.

---
+++ 45_enums/src/Main.java
@@ -0,0 +1,22 @@
+
+public class Main {
+	
+	public static void main(String[] args) {
+		UserStatus status = UserStatus.ACTIVE;
+
+		switch(status){
+		case PENDING:
+			System.out.println("Wait for few more minutes.");
+			break;
+		case ACTIVE:
+			System.out.println("No problem, You can pass.");
+			break;
+		case BLOCKED:
+			System.out.println("You can't pass");
+			break;
+			default:
+				System.out.println("Unsupported enums");
+		}
+	}
+	
+}
---
+++ 45_enums/src/UserStatus.java
@@ -0,0 +1,7 @@
+
+public enum UserStatus {
+	PENDING, ACTIVE, BLOCKED;
+
+
+}
+
