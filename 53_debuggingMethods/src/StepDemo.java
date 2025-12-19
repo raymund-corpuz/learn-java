053267c0db07d66b341abf60b369bebd7311ced2
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-12-19 21:28:02 +0800

    53_debuggingMethods: StepDemo

---
+++ 53_debuggingMethods/src/StepDemo.java
@@ -0,0 +1,15 @@
+
+public class StepDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int result = calculateSum(5, 10);
+		System.out.println("The sum is: " + result);
+	}
+	
+	public static int calculateSum(int a, int b) {
+		System.out.println("Calculating the sum...");
+		return a + b;
+	}
+
+}
