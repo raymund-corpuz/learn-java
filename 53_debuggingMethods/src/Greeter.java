65ea4682a41a4fb0004fc9bd03c421a8468ab02a
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-12-19 21:05:24 +0800

    53_debuggingMetods: Greeter Class.

---
+++ 53_debuggingMethods/src/Greeter.java
@@ -0,0 +1,11 @@
+
+public class Greeter {
+	public static void main(String[] args) {
+		greetUser("Alice");
+	}
+	
+	public static void greetUser(String name) {
+		String user = name;
+		System.out.println("Hi Good Evening "+ user);
+	}
+}
