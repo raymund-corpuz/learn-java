6d250832be819575f6cb626a98156611495829b8
raymund-corpuz <raymundcorpuz86@gmail.com>
2026-05-09 10:32:45 +0800

    03_Printing Data: Concatenating of Data

---
+++ 01_test/src/ConcatenateData.java
@@ -0,0 +1,14 @@
+
+public class ConcatenateData {
+	public static void main(String[] args) {
+		
+		String fName = "John";
+		char emailSymbol = '@';
+		String emailExtension = "gmail.com";
+		
+		System.out.println("Hello " + fName + "!");
+		System.out.println("Here is your email address");
+		System.out.println(fName.toLowerCase() + emailSymbol + emailExtension);
+	}
+
+}
