0d27c02ca5028d82e0e12c9cdcbff8d3ff51907c
raymund-corpuz <raymundcorpuz86@gmail.com>
2026-05-09 10:27:52 +0800

    03_Printing Data: Printing all the data using the println and print keyword.

---
+++ 01_test/src/PrintData.java
@@ -0,0 +1,12 @@
+
+public class PrintData {
+	public static void main(String[] args) {
+		System.out.print("1");
+		System.out.print("0");
+		System.out.print("08c");
+		System.out.println("Q3");
+		System.out.print("22");
+		System.out.print("E81");
+	}
+
+}
