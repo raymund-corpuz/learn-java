c71e6cc5be4afda081f5e7155dc7f6dbdd110b40
raymund-corpuz <raymundcorpuz86@gmail.com>
2026-05-11 20:27:56 +0800

    05_Using Scanner:  Creating a program that will ask the user if he wants food until the user says no.

---
+++ 01_test/src/MoreFood.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class MoreFood {
+	
+public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+	
+	for(;;) {
+		System.out.println("Do you want some food? ");
+		String answer = input.nextLine();
+		if(answer.equals("yes")) {
+			System.out.println("Here you go!");
+		}else if(answer.equals("no")){
+			System.out.println("Ok... :(");
+			break;
+		}
+		
+	}
+	input.close();
+	
+}
+}
