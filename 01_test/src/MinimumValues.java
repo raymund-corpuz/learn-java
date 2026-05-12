5f0183834ff12fd8a42d0ec9b30c12df294c08b8
raymund-corpuz <raymundcorpuz86@gmail.com>
2026-05-12 08:22:32 +0800

    05_Minimum Values: Create a program that displays the minimum values of different types.

---
+++ 01_test/src/MinimumValues.java
@@ -0,0 +1,20 @@
+
+public class MinimumValues {
+	public static void main(String[] args) {
+		byte smallestByte = Byte.MIN_VALUE;
+		short smallestShort = Short.MIN_VALUE;
+		int smallestInt = Integer.MIN_VALUE;
+		long smallestLong = Long.MIN_VALUE;
+		float smallestFloat = Float.MIN_VALUE;
+		double smallestDouble = Double.MIN_VALUE;
+		
+		System.out.println("Minimum byte value: " + smallestByte);
+		System.out.println("Minimum short value: " + smallestShort);
+		System.out.println("Minimum integer value: " + smallestInt);
+		System.out.println("Minimum long value: " + smallestLong);
+		System.out.println("Minimum float value: " + smallestFloat);
+		System.out.println("Minimum double value: " + smallestDouble);
+		
+		
+	}
+}
