1242b0c33088907704cdabe59b29c3038ae67a0f
raymund-corpuz <raymundcorpuz86@gmail.com>
2026-05-14 10:02:04 +0800

    07_namingVariable: Creating a simple Weather app and displays proper dataType.

---
+++ 01_test/src/WeatherApp.java
@@ -0,0 +1,13 @@
+
+public class WeatherApp {
+	public static void main(String[] args) {
+		double temperatureCelsius = 25.5;
+		int humidityPercentage = 60;
+		double windSpeedKmph = 15.8;
+		
+		System.out.printf("Temperature: %.1f C\n", temperatureCelsius);
+		System.out.printf("Humidity: %d%%\n", humidityPercentage);
+		System.out.printf("Wind Speed: %.1f km/h\n", windSpeedKmph);
+			
+	}
+}
