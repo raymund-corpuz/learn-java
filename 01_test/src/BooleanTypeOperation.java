d334c0fe56cf09321ee9f11328a0e6fd265059bd
raymund-corpuz <raymundcorpuz86@gmail.com>
2026-05-20 07:36:24 +0800

    Move to IntelliJ

---
+++ 01_test/src/BooleanTypeOperation.java
@@ -0,0 +1,10 @@
+public class BooleanTypeOperation{
+    public static void main(String[] args){
+
+        boolean isOpen = true;
+        boolean isClose = false;
+
+        System.out.println("Is the door open ?" + isOpen);
+        System.out.println("Is the door open?" + isClose);
+    }
+}
